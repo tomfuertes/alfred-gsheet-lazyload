ad6bf2f8e9de0d9e072c8049df76fcc0c6f42dd2
Tom Fuertes <tomfuertes@gmail.com>
2016-03-29 17:02:30 -0500

    add filesystem

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
 var alfredo = require('alfredo');
 var Item = alfredo.Item;
 var GoogleSpreadsheet = require("google-spreadsheet");
-var _ = require('underscore');
+var lodash = require('lodash');
 
+// var sheetId = "1JHtId-vYNVT9q6U8QErd4ksXxhTkc3ua2snWfUiOmmQ";
 var sheetId = process.argv[2];
-var keyword = process.argv[3];
+var shorthand = process.argv[2];
+
+var fs = require('fs');
+
+var cacheFileName = './data/gsheet-cache.json';
+var cachedDataRows = require(cacheFileName);
 
 // spreadsheet key is the long id in the sheets URL
 var sheet = new GoogleSpreadsheet(sheetId);
 
-sheet.getRows(1, {}, function (err, rows) {
-	if (err) throw err;
+sheet.getRows(1, {}, function(err, rows) { // x = rows});
+  if (err) throw err;
 
-	var items = [];
+  var toCache = [];
 
-	var matches = _.chain(rows)
-		.filter(function (row) {
-			return row.keyword.startsWith(keyword);
-		})
-		.each(function (row) {
-			var item = new Item({
-				arg: row.links,
-				title: row.notes,
-				subtitle: row.links,
-				valid: true,
-				autocomplete: row.keyword
-			});
+  lodash.chain(rows).filter(function(row) {
+    return true; // row.shorthand.startsWith(shorthand);
+  }).each(function(row) {
+    var item = {
+      links: row.links,
+      notes: row.notes,
+      shorthand: row.shorthand
+    };
+    toCache.push(item);
+  }).value();
 
-			items.push(item);
-		})
-		.value();
+  fs.writeFile(cacheFileName, JSON.stringify(toCache, null, 2), function(err) {
+    if (err) {
+      console.error(err);
+      throw new Error(err);
+    }
+  });
 
-	alfredo.feedback(items);
 });
+
+var items = [];
+
+lodash.chain(cachedDataRows).filter(function(row) {
+  return row.shorthand.startsWith(shorthand);
+}).each(function(row) {
+  var item = new Item({
+    arg: row.links,
+    title: row.notes,
+    subtitle: row.links,
+    valid: true,
+    autocomplete: row.shorthand
+  });
+  items.push(item);
+}).value();
+
+alfredo.feedback(items);
